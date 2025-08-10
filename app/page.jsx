aa606fd756a58cf2da4e63db6fb8e4b1e400f2e9
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-08-10 20:57:37 +0600

    feat: add necessary components

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { ToggleTheme } from "@/components/ui/ThemeToggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ToggleTheme></ToggleTheme>
+    </div>
+  );
+}
