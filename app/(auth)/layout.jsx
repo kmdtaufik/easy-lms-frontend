16da261628bc6c9d1e97b56df11ed5b41f5374e5
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-08-22 15:49:20 +0600

    feat: auth layout, vefiy page

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,9 +1,46 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
+import Logo from "@/public/logo.png";
 export default function AuthLayout({ children }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <Link
+        href={"/"}
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute left-4 top-4",
+        })}
+      >
+        {" "}
+        <ArrowLeft></ArrowLeft>
+        Back
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          {" "}
+          <Image src={Logo} height={32} width={32} alt="logo" />
+          EasyLMS
+        </Link>
+        {children}
+
+        <div className="text-sm text-balance text-center text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link href={"#"} className="hover:text-primary">
+            Terms of Services
+          </Link>{" "}
+          &{" "}
+          <Link href={"#"} className="hover:text-primary">
+            Privacy Policy
+          </Link>
+          .{" "}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/(auth)/verify-email/page.jsx
@@ -0,0 +1,95 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { authClient } from "@/lib/auth";
+import { Loader2 } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
+export default function VerifyRequest() {
+  const [otp, setOtp] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const params = useSearchParams();
+  const email = params.get("email");
+  const isOTPValid = otp.length === 6;
+  // Function to handle OTP verification
+  function verifyOtp() {
+    startTransition(async () => {
+      await authClient.signIn.emailOtp({
+        email: email,
+        otp: otp,
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("OTP verified successfully!");
+            router.push("/");
+          },
+          onError: (error) => {
+            toast.error(`OTP verification failed.`);
+          },
+        },
+      });
+    });
+  }
+  return (
+    <Card className={"w-full mx-auto"}>
+      <CardHeader className={"text-center"}>
+        <CardTitle className={"text-xl"}>Please check your email</CardTitle>
+        <CardDescription>
+          We have sent you a verification email. Please check your inbox.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className={"space-y-6"}>
+        <div className="flex flex-col items-center space-y-2">
+          <InputOTP
+            maxLength={6}
+            className="gap-2"
+            value={otp}
+            onChange={(value) => setOtp(value)}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0}></InputOTPSlot>
+              <InputOTPSlot index={1}></InputOTPSlot>
+              <InputOTPSlot index={2}></InputOTPSlot>
+            </InputOTPGroup>
+            <InputOTPSeparator></InputOTPSeparator>
+            <InputOTPGroup>
+              {" "}
+              <InputOTPSlot index={3}></InputOTPSlot>
+              <InputOTPSlot index={4}></InputOTPSlot>
+              <InputOTPSlot index={5}></InputOTPSlot>
+            </InputOTPGroup>
+          </InputOTP>
+          <p className="text-sm text-muted-foreground">
+            Enter your 6-digit code.
+          </p>
+        </div>
+        <Button
+          className={"w-full"}
+          onClick={verifyOtp}
+          disabled={isPending || !isOTPValid}
+        >
+          {isPending ? (
+            <>
+              <Loader2 className="size-4 animate-spin"></Loader2>
+            </>
+          ) : (
+            "Verify Request"
+          )}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
