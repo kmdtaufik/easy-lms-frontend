8d133b62697c0bf4ce18cbb353e0b736c27517b8
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-08-28 16:14:23 +0600

    feat: add suspense loader

--- app/admin/courses/page.jsx
+++ app/admin/courses/page.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { AdminCourseCard } from "./_components/AdminCourseCard";
 import EmptyState from "@/components/genral/EmptyState";
 import { PlusIcon } from "lucide-react";
+import { Suspense } from "react";
+import CSSLoader from "@/components/loader/loader";
 
 export default async function CoursesPage() {
   let page = 1;
@@ -33,11 +35,13 @@ export default async function CoursesPage() {
               <PlusIcon className="size-4" /> Create Course
             </Link>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2  gap-6">
-            {data.map((course) => (
-              <AdminCourseCard course={course} key={course.id} />
-            ))}
-          </div>
+          <Suspense fallback={<CSSLoader variant="default" size="md" />}>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  gap-6">
+              {data.map((course) => (
+                <AdminCourseCard course={course} key={course.id} />
+              ))}
+            </div>
+          </Suspense>
         </>
       ) : (
         <EmptyState />
