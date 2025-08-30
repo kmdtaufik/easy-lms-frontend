8c1d4321f8846abc30a1338be24fdec19f44a108
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-08-30 11:41:08 +0600

    fix: add footer

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -1,3 +1,4 @@
+import Footer from "./_components/Footer";
 import Navbar from "./_components/Navbar";
 
 export default function PublicLayout({ children }) {
@@ -7,6 +8,7 @@ export default function PublicLayout({ children }) {
       <main className="container mx-auto px-4 md:px-6 lg:px-8 mb-32">
         {children}
       </main>
+      <Footer />
     </div>
   );
 }
