1d65224f6d5fbf1df9b2c8429da6edd83bc2322d
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-08-30 14:38:58 +0600

    fix: api url

--- lib/user.jsx
+++ lib/user.jsx
@@ -2,15 +2,18 @@
 import axios from "axios";
 export const fetchSession = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/api/me", {
-      withCredentials: true, // important to send cookies
-    });
+    const response = await axios.get(
+      "https://easy-lms-backend.vercel.app/api/me",
+      {
+        withCredentials: true, // important to send cookies
+      },
+    );
 
     return response.data;
   } catch (error) {
     console.error(
       "Failed to get session:",
-      error.response?.data || error.message || error
+      error.response?.data || error.message || error,
     );
     return null;
   }
